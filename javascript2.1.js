fd32c8cf93052bd16438572474246f92129fcc2e
Muddyweavy <54936267+Muddyweavy@users.noreply.github.com>
2020-01-15 09:06:32 +0100

    Initial commit

---
+++ javascript2.1.js
@@ -0,0 +1,9 @@
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+
+document.getElementById("subkop").innerText = "Mijn geweldige subkop";
+
+document.getElementById("aap1").src = "WWIII.png";
+
+document.getElementById("aap2").src = "Image2.png";
+
+paginetitel.style.backgroundColor = "red";
