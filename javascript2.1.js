2108901ab445f999af71724e2ebace27aa4b9bb9
Muddyweavy <54936267+Muddyweavy@users.noreply.github.com>
2020-02-17 13:46:34 +0100

    Add files via upload

--- javascript2.1.js
+++ javascript2.1.js
@@ -1,9 +1,9 @@
 document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+paginatitel.style.backgroundColor = "red";
 
 document.getElementById("subkop").innerText = "Mijn geweldige subkop";
+subkop.style.backgroundColor = "yellow";
 
 document.getElementById("aap1").src = "WWIII.png";
 
 document.getElementById("aap2").src = "Image2.png";
-
-paginetitel.style.backgroundColor = "red";
